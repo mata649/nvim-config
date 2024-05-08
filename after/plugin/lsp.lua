feb689b6f9e32fbed0b9a55d02a66926689ba7cc
Jose Mata <mata649@hotmail.com>
2024-05-08 17:21:37 -0600

    finally perfection

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -51,7 +51,8 @@ local cmp = require('cmp')
       ['<C-Space>'] = cmp.mapping.complete(),
       ['<C-e>'] = cmp.mapping.abort(),
       ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    }),
+      ['<Tab>'] = cmp.mapping.confirm({ select = true }),     
+  }),
     sources = cmp.config.sources({
       { name = 'nvim_lsp' },
       -- { name = 'vsnip' }, -- For vsnip users.
@@ -62,5 +63,3 @@ local cmp = require('cmp')
       { name = 'buffer' },
     })
   })
-   
-
--- lua/mata649/packer.lua
+++ lua/mata649/packer.lua
@@ -10,7 +10,6 @@ return require('packer').startup(function(use)
   use {
   'nvim-telescope/telescope.nvim', tag = '0.1.6',
   requires = { {'nvim-lua/plenary.nvim'} }
-  
 }
   use { "catppuccin/nvim", as = "catppuccin" }
 
@@ -30,3 +29,4 @@ return require('packer').startup(function(use)
   }
 }
 end)
+
