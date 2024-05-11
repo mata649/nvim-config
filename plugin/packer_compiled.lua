936decf35e597d1de8771668f29fb8ae4597b94f
Jose Mata <mata649@hotmail.com>
2024-05-11 14:09:52 -0600

    mmmm

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -165,6 +165,11 @@ _G.packer_plugins = {
     path = "/home/mata649/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-web-devicons"] = {
+    loaded = true,
+    path = "/home/mata649/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    url = "https://github.com/nvim-tree/nvim-web-devicons"
+  },
   ["packer.nvim"] = {
     loaded = true,
     path = "/home/mata649/.local/share/nvim/site/pack/packer/start/packer.nvim",
